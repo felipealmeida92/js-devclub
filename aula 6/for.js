f20fef48a6dcaacb724622df7494f1a7a5925fda
felipealmeida92 <129707274+felipealmeida92@users.noreply.github.com>
2024-06-01 21:32:25 -0300

    update aula 6

---
+++ aula 6/for.js
@@ -0,0 +1,5 @@
+const users = ['Maria', 'Aline', 'João']
+
+for(let i = 0; i > users.length; i++){
+    console.log(users[i])
+}
---
+++ aula 6/node_be4195e606a3b.tmp.js
@@ -0,0 +1,5 @@
+const users = ['Maria', 'Aline', 'João']
+
+for(let i = 0; users.length; i++){
+    console.log(users[i])
+}
